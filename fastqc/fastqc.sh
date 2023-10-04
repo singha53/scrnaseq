910c2ac95f856231fabe9ab6ba7c9378f7756825
singha53 <amrit.singh@hli.ubc.ca>
2023-10-04 12:57:21 -0700

    update fastqc sockeye scripts

--- fastqc/fastqc.sh
+++ fastqc/fastqc.sh
@@ -4,5 +4,5 @@ cd $RUN_PATH
 for file in $(ls $RUN_PATH)
 do
     SAMPLE=`basename $file`
-    fastqc -t 5 ${SAMPLE} -o fastqc_results
+    fastqc -t 5 ${SAMPLE} -o /scratch/st-singha53-1/singha53/scrnaseq/fastqc/
 done
