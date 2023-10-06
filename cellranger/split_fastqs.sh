c1f05c2b8213610591ade00d2f0d8571731aa6e7
singha53 <amrit.singh@hli.ubc.ca>
2023-10-05 23:59:28 -0700

    cellranger

---
+++ cellranger/split_fastqs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+files=$(ls /project/st-singha53-1/datasets/fentanyl/scrnaseq)
+rats=(R15 R16 R3 R4)
+
+for key in ${!rats[@]}; do
+  printf -- '%s\n' "${files[@]}" | grep ${rats[$key]} > $key.txt
+done
